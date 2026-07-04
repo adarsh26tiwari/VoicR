51fd95fcaa4d0c146f49e62ad76aa5928334f85c
Adarsh Tiwari <adarsh.20243012@mnnit.ac.in>
2026-07-04 15:55:44 +0530

    Update CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 const privateCors = cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://voicr.onrender.com"],
     credentials: true
 });
 
